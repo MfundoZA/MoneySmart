c835aa10860208c78d0da64c153cac934b35f38e
Mfundo <jordantenza@gmai.com>
2022-06-30 21:30:01 +0200

    Created models

---
+++ MoneySmart/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneySmart.Models
+{
+    public class Transaction
+    {
+        public string Descrption { get; set; }
+        public Type Type { get; set; }
+        public double Amount { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+    }
+}
