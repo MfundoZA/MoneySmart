fd131ececa2b8a61b5ffe9c5b3a462b92dbd050f
MfundoZA <jordantenza@gmail.com>
2022-06-30 21:30:01 +0200

    Created models

---
+++ MoneySmart/Models/PaymentMethod.cs
@@ -0,0 +1,9 @@
+﻿namespace MoneySmart.Models
+{
+    public enum PaymentMethod
+    {
+        Cash,
+        Card,
+        EFT
+    }
+}
---
+++ MoneySmart/Models/Type.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneySmart.Models
+{
+    public enum Type
+    {
+        Income,
+        Expense
+    }
+}
