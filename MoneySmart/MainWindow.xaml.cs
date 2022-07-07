a15b47c55307461a7c73287a1182548c895f903b
MfundoZA <jordantenza@gmail.com>
2022-07-07 11:20:56 +0200

    Created a viewmodel for the mainwindow

--- MoneySmart/MainWindow.xaml.cs
+++ MoneySmart/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoneySmart.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace MoneySmart
         public MainWindow()
         {
             InitializeComponent();
+            MainViewModel viewModel = new MainViewModel();
         }
     }
 }
---
+++ MoneySmart/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MoneySmart.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneySmart.ViewModel
+{
+    class MainViewModel
+    {
+        private Database database { get; set; }
+
+        public MainViewModel()
+        {
+            database = new Database();
+        }
+    }
+}
