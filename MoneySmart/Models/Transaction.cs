2b7e04e66908bc2d18bf207c91a85ff52a561782
MfundoZA <jordantenza@gmail.com>
2022-07-25 14:23:27 +0200

    Created a constructor that accepts description, type, amount and paymentMethod as parameters

--- MoneySmart/Models/Transaction.cs
+++ MoneySmart/Models/Transaction.cs
@@ -7,9 +7,22 @@ namespace MoneySmart.Models
     public class Transaction
     {
         public int Id { get; set; }
-        public string Descrption { get; set; }
+        public string Description { get; set; }
         public Type Type { get; set; }
         public decimal Amount { get; set; }
         public PaymentMethod PaymentMethod { get; set; }
+
+        public Transaction()
+        {
+
+        }
+
+        public Transaction(string description, Type type, decimal amount, PaymentMethod paymentMethod)
+        {
+            Description = description;
+            Type = type;
+            Amount = amount;
+            PaymentMethod = paymentMethod;
+        }
     }
 }
