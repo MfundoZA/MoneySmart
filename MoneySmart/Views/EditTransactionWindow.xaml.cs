76392cbc653ebf5494ba1b5518e556622611f491
Mfundo <jordantenza@gmail.com>
2023-10-31 08:10:02 +0200

    Created onclick events for the EditTransaction and Cancel buttons

---
+++ MoneySmart/Views/EditTransactionWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MoneySmart.Views
+{
+    /// <summary>
+    /// Interaction logic for EditTransactionWindow.xaml
+    /// </summary>
+    public partial class EditTransactionWindow : Window
+    {
+        public EditTransactionWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEditTransaction_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
